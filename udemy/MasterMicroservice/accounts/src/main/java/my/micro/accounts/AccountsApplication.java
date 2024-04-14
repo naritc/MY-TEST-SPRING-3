83787431a4e9731b2672fed0fbef84e63bb8cba9
Narit Chaichoochal <o_narit.c@kbtg.tech>
2024-04-15 04:10:59 +0700

    eureka !!

--- udemy/MasterMicroservice/accounts/src/main/java/my/micro/accounts/AccountsApplication.java
+++ udemy/MasterMicroservice/accounts/src/main/java/my/micro/accounts/AccountsApplication.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
@@ -34,6 +35,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @ConfigurationPropertiesScan(basePackages = "my.micro.accounts.config.properties")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AccountsApplication {
 
